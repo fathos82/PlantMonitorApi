32bda8ce24869e3cf9ca4a60765a7621472d93f9
Athos <fathos82@gmail.com>
2026-03-23 02:26:52 -0300

    feat: desactivating security.

--- src/main/java/io/athos/agrocore/plantmonitor/PlantMonitorApplication.java
+++ src/main/java/io/athos/agrocore/plantmonitor/PlantMonitorApplication.java
@@ -2,10 +2,16 @@ package io.athos.agrocore.plantmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
+})
+
 public class PlantMonitorApplication {
 
     static void main(String[] args) {
