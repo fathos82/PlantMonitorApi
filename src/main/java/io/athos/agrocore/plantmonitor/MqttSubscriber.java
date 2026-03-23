2de770aa86eb947a406b810060b026e1d96ffc14
Athos <fathos82@gmail.com>
2026-03-23 17:42:22 -0300

    feat: desactivating security.

--- src/main/java/io/athos/agrocore/plantmonitor/MqttSubscriber.java
+++ src/main/java/io/athos/agrocore/plantmonitor/MqttSubscriber.java
@@ -3,6 +3,7 @@ package io.athos.agrocore.plantmonitor;
 import io.athos.agrocore.plantmonitor.monitorings.measurement.MeasurementService;
 import jakarta.annotation.PostConstruct;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,17 +20,23 @@ public class MqttSubscriber {
     @Autowired
     MeasurementService measurementService;
 
-    @Value("${mqtt.server.url:tcp://localhost:1883}")
+    @Value("${mqtt.server.url:tcp://147.93.176.117:1883}")
     private String serverURL;
 
 
+
+
     @PostConstruct
-    public void subscribe() throws MqttException, MqttException {
+    public void subscribe() throws MqttException {
 
-        MqttClient client =
-                new MqttClient(serverURL, "plant_monitor_api-client");
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
 
-        client.connect();
+        MqttClient client = new MqttClient(serverURL, "plant_monitor-client");
+        client.connect(options);
+
         client.subscribe("plant/data", (topic, msg) -> {
             String payload = new String(msg.getPayload(), StandardCharsets.UTF_8);
             JsonNode root = objectMapper.readTree(payload);
