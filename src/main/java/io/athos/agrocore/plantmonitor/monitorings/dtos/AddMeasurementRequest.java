6076227da5587647f89fccba38a83304dbfef4df
Athos <fathos82@gmail.com>
2026-03-28 20:15:48 -0300

    feat:  update all.

--- src/main/java/io/athos/agrocore/plantmonitor/monitorings/dtos/AddMeasurementRequest.java
+++ src/main/java/io/athos/agrocore/plantmonitor/monitorings/dtos/AddMeasurementRequest.java
@@ -1,10 +1,14 @@
 package io.athos.agrocore.plantmonitor.monitorings.dtos;
 
 import io.athos.agrocore.plantmonitor.monitorings.measurement.MeasurementType;
+import jakarta.validation.constraints.NotNull;
 
 public record AddMeasurementRequest(
+        @NotNull
         Long plantId,
+        @NotNull
         MeasurementType measurementType,
+        @NotNull
         Long sensorId
 ) {
 }
