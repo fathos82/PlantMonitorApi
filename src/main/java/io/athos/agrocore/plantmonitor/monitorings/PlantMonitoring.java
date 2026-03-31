b00b9547e46281b99fc47358879086392c3668f2
Athos <fathos82@gmail.com>
2026-03-30 22:02:50 -0300

    feat:  update all.

--- src/main/java/io/athos/agrocore/plantmonitor/monitorings/PlantMonitoring.java
+++ src/main/java/io/athos/agrocore/plantmonitor/monitorings/PlantMonitoring.java
@@ -24,7 +24,7 @@ public class PlantMonitoring {
 
     @Column(nullable = false)
     private String specieName;
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Measurement> measurements;
 
 
