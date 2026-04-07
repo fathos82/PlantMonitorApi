7dd527eb5bac451e09f6286bbea5b13c4203ff73
Athos <fathos82@gmail.com>
2026-04-06 23:59:17 -0300

    feat:  update all.

--- src/main/java/io/athos/agrocore/plantmonitor/configuration/TimescaleConfig.java
+++ src/main/java/io/athos/agrocore/plantmonitor/configuration/TimescaleConfig.java
@@ -15,6 +15,7 @@ public class TimescaleConfig {
     @PostConstruct
     public void init() {
         jdbc.execute("CREATE EXTENSION IF NOT EXISTS timescaledb;");
+        jdbc.execute("CREATE EXTENSION IF NOT EXISTS timescaledb_toolkit;");
         jdbc.execute("SELECT create_hypertable('measurement_value', 'timestamp', migrate_data => true, if_not_exists => TRUE);");
     }
 
