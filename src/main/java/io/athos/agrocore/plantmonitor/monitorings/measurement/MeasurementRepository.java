0075c5bcb3518e3bea8b2ac894ed035ed8ce76ee
Athos <fathos82@gmail.com>
2026-04-07 22:38:26 -0300

    feat:  update all.

--- src/main/java/io/athos/agrocore/plantmonitor/monitorings/measurement/MeasurementRepository.java
+++ src/main/java/io/athos/agrocore/plantmonitor/monitorings/measurement/MeasurementRepository.java
@@ -21,56 +21,7 @@ public interface MeasurementRepository extends JpaRepository<Measurement, Long> 
     @Query("UPDATE Measurement m SET m.virtualSensor = null WHERE m.virtualSensor.id = :sensorId AND m.plantMonitoring.user.id" +
             "= :userId")
     void detachSensorFromMeasurements(@Param("sensorId") Long sensorId, @Param("userId") Long userId);
-    @Query("""
-        select distinct m
-        from Measurement m
-        left join fetch m.values v
-        where m.plantMonitoring.id = :plantId
-        and m.measurementType = : measurementType
-        order by v.timestamp asc
-    """)
-    List<Measurement> findByPlantMonitoring_IdAndMeasurementTypeOrder_ValueByTimestampAsc(
-            @Param("plantId") Long plantId,
-            @Param("measurementType") MeasurementType measurementType
-    );
 
-    @Query("""
-        select distinct m
-        from Measurement m
-        left join fetch m.values v
-        where m.plantMonitoring.id = :plantId
-        order by v.timestamp asc
-    """)
-    List<Measurement> findByPlantMonitoring_IdOrder_ValueByTimestampAsc(
-            @Param("plantId") Long plantId);
-
-    @Query("""
-        select distinct m
-        from Measurement m
-        left join fetch m.values v
-        where m.plantMonitoring.id = :plantId
-          and (v is null or v.timestamp >= :from)
-        order by v.timestamp asc
-    """)
-    List<Measurement> findByPlant_IdAndValuesAfter(
-            @Param("plantId") Long plantId,
-            @Param("from") Instant from
-    );
-
-    @Query("""
-        select distinct m
-        from Measurement m
-        left join fetch m.values v
-        where m.plantMonitoring.id = :plantId
-          and m.measurementType = :measurementType
-          and (v is null or v.timestamp >= :from)
-        order by v.timestamp asc
-    """)
-    List<Measurement> findByPlant_IdAndMeasurementTypeAndValuesAfter(
-            @Param("plantId") Long plantId,
-            @Param("measurementType") MeasurementType measurementType,
-            @Param("from") Instant from
-    );
 
     Optional<Measurement> findByIdAndVirtualSensor_Device_User_Id(Long id, Long virtualSensorDeviceUserId);
 
@@ -78,6 +29,4 @@ public interface MeasurementRepository extends JpaRepository<Measurement, Long> 
     void delete(Measurement entity);
 
     List<Measurement> findAllByPlantMonitoring_User_Id(Long id);
-
-    void deleteAllByPlantMonitoring_Id(Long plantMonitoringId);
 }
