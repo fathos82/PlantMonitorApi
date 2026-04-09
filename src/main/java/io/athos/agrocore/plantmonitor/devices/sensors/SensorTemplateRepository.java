52b9b1b98c2493b86eaa7e3b41a5007513ac5679
Athos <fathos82@gmail.com>
2026-04-08 22:43:14 -0300

    feat:  update all.

--- src/main/java/io/athos/agrocore/plantmonitor/devices/sensors/SensorTemplateRepository.java
+++ src/main/java/io/athos/agrocore/plantmonitor/devices/sensors/SensorTemplateRepository.java
@@ -1,9 +1,6 @@
 package io.athos.agrocore.plantmonitor.devices.sensors;
-
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface SensorTemplateRepository extends JpaRepository<SensorTemplate, Long> {
